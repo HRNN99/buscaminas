93381f2d9deba1abb55418f226e7646b27e4a731
francocapu <caputo.franco125@gmail.com>
2025-05-12 19:41:53 -0300

    comprobacion de memoria y liberacion de memoria al final

--- dibujos.h
+++ dibujos.h
@@ -18,4 +18,5 @@ void borrarPantalla(SDL_Window *ventana, SDL_Renderer *renderer);
 void CrearMapa(int* mapa, int minas, int filas, int columnas);
 void _ReiniciarMapa(int* mapa, int filas, int columnas);
 int _ColocarMinas(int posMinas[][2], int fil,int* x, int* y);
+void FinalizarSDL(SDL_Window *ventana, SDL_Renderer *renderer, int estadoExit, int* mapa);
 #endif // DIBUJOS_H_INCLUDED
