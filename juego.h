e128b82a99a618da50111531d75645eb801a9ef2
Frame3M <ortegamarco.it@gmail.com>
2025-05-17 22:39:16 -0300

    agregado de header y .c de juego

---
+++ juego.h
@@ -0,0 +1,15 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include "dibujos.h"
+
+#define eleccion(n) ((n) == 0 ? square2 : (n) == 1 ? one : (n) == 2 ? two : (n) == 3 ? three : mine)
+
+
+//Prototipos
+void fondoColor(SDL_Renderer* renderer);
+bool casillaColocacion(SDL_Renderer* renderer);
+void casillaEstado(SDL_Renderer* renderer , Casilla mapa[][TAM_GRILLA] , int xGrilla , int yGrilla);
+bool casillaBandera(SDL_Renderer* renderer, int xGrilla , int yGrilla);
+
+#endif // JUEGO_H
