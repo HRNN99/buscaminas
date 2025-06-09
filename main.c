95ee9715fa6beae18ab0db9702b54db37ea48468
Hernan <villahrnn@hotmail.com>
2025-06-09 11:29:46 -0300

    fondo, marco y sin borde ventana de ganado

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
     // String formateado para el titulo de ventana
     sprintf(nombreVentana, "Buscaminas %ix%i", filas, columnas);
     // Tamaño de ancho y altura de la ventana, utilizo 1 sola variable ya que sera cuadrada
-    int TAMX =  TAM_PIXEL * (columnas * PIXELES_X_LADO + 20);
+    int TAMX =  TAM_PIXEL * (columnas * PIXELES_X_LADO + 2*8 + 4);
     int TAMY =  TAM_PIXEL * (filas * PIXELES_X_LADO + 4 + 3*8 + 28);
     // Funcion para crear ventana con posicion especifica, dimension y banderas.
     SDL_Window *ventana = SDL_CreateWindow(nombreVentana, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, TAMX, TAMY, 2);
@@ -57,6 +57,10 @@ int main(int argc, char *argv[])
     SDL_Renderer *renderer = SDL_CreateRenderer(ventana, -1, SDL_RENDERER_ACCELERATED);
     // Funcion para establecer el modo de mezcla de colores para el renderizado, el modo blend nos permite utilizar transparencia
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+    // Tamaño de ancho y altura de la ventana de ganado
+    int TAMX_GANADO = TAMX-100;
+    int TAMY_GANADO = TAMY-100;
 
     //////////////////////////////////////////////////////////////////////
 
@@ -135,15 +139,15 @@ int main(int argc, char *argv[])
             //Inicio la lectura de teclado
             SDL_StartTextInput();
             // Limpia pantalla
-            SDL_SetRenderDrawColor(rendererGanado, 0, 0, 0, 255); // negro
-            SDL_RenderClear(rendererGanado);
+            rectanguloLleno(rendererGanado, GS, 0, 0, TAMX_GANADO, TAMY_GANADO);
+            marco(rendererGanado, 0, 0, (TAMX_GANADO)/2, (TAMY_GANADO)/2, 2);
             // Renderizar "Puntaje" y "Nombre:"
             char textoPuntaje[21] = "Puntaje: ";
             char puntajeChar[12];
             strcat(textoPuntaje, itoa(juego.puntaje, puntajeChar, 10)); //Armado de String a imprimir
-            renderizarTexto(font, 24, textoPuntaje, BB, GS,rendererGanado, 50, 50);
-            renderizarTexto(font, 24, "Ingrese su nombre:", BB, GS, rendererGanado, 50, 100);
-            renderizarTexto(font, 24, nombreJugador, BB, GS,rendererGanado, 50, 120);
+            renderizarTexto(font, 24, textoPuntaje, BB, GS,rendererGanado, 20, 50);
+            renderizarTexto(font, 16, "Ingrese su nombre:", BB, GS, rendererGanado, 20, 100);
+            renderizarTexto(font, 16, nombreJugador, BB, GS,rendererGanado, 20, 130);
 
             // Mostrar todo
             SDL_RenderPresent(rendererGanado);
@@ -205,7 +209,7 @@ int main(int argc, char *argv[])
                             renderizarGanado = 1;
                             *nombreJugador = '\0'; // Limpieza por si se presionaron teclas al jugar
                             // Funcion para crear ventana con posicion especifica, dimension y banderas.
-                            ventanaGanado = SDL_CreateWindow("Ganaste!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, TAMX - 100, TAMY - 100, 2);
+                            ventanaGanado = SDL_CreateWindow("Ganaste!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, TAMX_GANADO, TAMY_GANADO, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS);
                             // Funcion para crear el renderizado en ventana acelerado por hardware
                             rendererGanado = SDL_CreateRenderer(ventanaGanado, -1, SDL_RENDERER_ACCELERATED);
                             // Funcion para establecer el modo de mezcla de colores para el renderizado, el modo blend nos permite utilizar transparencia
@@ -325,7 +329,7 @@ int escribirArchivoLog(FILE* archivoLog, Log* log)
     {
         fprintf(archivoLog, "[%d-%d-%d %02d:%02d:%02d] %-15s | coordenadas: (%d , %d)\n",
             log->fechaHora.tm_year + 1900, log->fechaHora.tm_mon + 1, log->fechaHora.tm_mday,
-            log->fechaHora.tm_hour, log->fechaHora.tm_min, log->fechaHora.tm_sec, log->tipoEvento, 
+            log->fechaHora.tm_hour, log->fechaHora.tm_min, log->fechaHora.tm_sec, log->tipoEvento,
             log->coordXY[0], log->coordXY[1]);
     }
     return 0;
